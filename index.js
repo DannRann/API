47f6ee677e22ebfa94516a485d8a83e4a21c9fd2
DannRann <132697954+DannRann@users.noreply.github.com>
2023-08-04 13:59:59 +0000

    add

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 import dotenv from "dotenv";
 dotenv.config();
 import { selectUsuarios } from "./bd.js";
+
+import express from "express";      // Requisição do pacote do express
+const app = express();              // Instancia o Express
+const port = 3000;                  // Define a porta
+
 app.get("/usuarios", async (req, res) => {
   console.log("Rota GET/usuarios solicitada");
   try {
@@ -11,9 +16,6 @@ app.get("/usuarios", async (req, res) => {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
 });
-import express from "express";      // Requisição do pacote do express
-const app = express();              // Instancia o Express
-const port = 3000;                  // Define a porta
 
 app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
   res.json({
