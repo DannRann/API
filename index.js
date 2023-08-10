102f3dc98f07aff48fe4bfaad37f4daa80a38872
DannRann <132697954+DannRann@users.noreply.github.com>
2023-08-10 11:43:00 +0000

    add

--- index.js
+++ index.js
@@ -28,3 +28,15 @@ app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
 app.listen(port, () => {            // Um socket para "escutar" as requisições
   console.log(`Serviço escutando na porta:  ${port}`);
 });
+import { selectUsuarios, selectUsuario } from "./bd.js";
+
+app.get("/usuario/:id", async (req, res) => {
+  console.log("Rota GET /usuario solicitada");
+  try {
+    const usuario = await selectUsuario(req.params.id);
+    if (usuario.length > 0) res.json(usuario);
+    else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
