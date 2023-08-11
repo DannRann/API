d07a470b6a9905c891a183de2c86165adc805798
DannRann <132697954+DannRann@users.noreply.github.com>
2023-08-11 13:23:03 +0000

    add

--- bd.js
+++ bd.js
@@ -34,4 +34,12 @@ async function deleteUsuario(id) {
   const query = "DELETE FROM usuario WHERE id = $1";
   await client.query(query, [id]);
 }
-export { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario };
+async function updateUsuario(data) {
+  const client = await connect();
+  const query =
+    "UPDATE usuario SET nome = $1, email = $2, senha = $3 WHERE id = $4";
+  const usuario = [data.nome, data.email, data.senha, data.id];
+  await client.query(query, usuario);
+}
+//bd.js
+export { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario, updateUsuario };
